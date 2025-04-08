0f53f952a285c0d09427f8f7fe2824a330b33aaa
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-04-08 18:10:41 +0600

    ready to submit

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import React from "react";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://green-link-server.vercel.app/",
   withCredentials: true,
 });
 
